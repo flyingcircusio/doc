159e01a92b2f6434d2651fb68b8ce87fa18f82d6
Christian Kauhaus <kc@flyingcircus.io>
2018-11-27 16:18:22 +0100

    Mention firewall rule display
    Also fix sphinx docs and add default.nix for easy local builds.

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  buildEnv = pkgs.python.withPackages (ps: [ ps.sphinx ps.sphinx_rtd_theme ]);
+
+in pkgs.stdenv.mkDerivation {
+  name = "flyingcircus-docs";
+  configurePhase = ":";
+  buildInputs = [ buildEnv ] ++ (with pkgs; [ python2 mercurial ]);
+  doCheck = false;
+  buildPhase = "sphinx-build -j 10 -a -b html $src/src $out";
+  installPhase = ":";
+  src = ./.;
+  dontStrip = true;
+  dontPatchELF = true;
+}
