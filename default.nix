b429b7acfd1e7e5ce08503028e64f4e59eb3e08c
Christian Kauhaus <kc@flyingcircus.io>
2020-11-30 11:37:39 +0100

    Update default.nix to Python3.8 as well

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  buildEnv = pkgs.python.withPackages (ps: [ ps.sphinx ps.sphinx_rtd_theme ]);
+  buildEnv = pkgs.python3.withPackages (ps: [ ps.sphinx ps.sphinx_rtd_theme ]);
 
 in pkgs.stdenv.mkDerivation {
   name = "flyingcircus-docs";
   configurePhase = ":";
-  buildInputs = [ buildEnv ] ++ (with pkgs; [ python2 git ]);
+  buildInputs = [ buildEnv ] ++ (with pkgs; [ python3 git ]);
   doCheck = false;
   buildPhase = "sphinx-build -j 10 -a -b html $src/src $out";
   installPhase = ":";
