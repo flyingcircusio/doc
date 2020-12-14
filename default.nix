e48d592beff7fed55aa37afdd12740f62b0c08b7
Christian Kauhaus <kc@flyingcircus.io>
2020-12-14 14:31:31 +0100

    Prepare for Hydra
    - Repeatable build: Use git commit id instead of timestamp
    - Add release.nix for Hydra job and declare build products
    
    PL-129471

--- default.nix
+++ default.nix
@@ -1,16 +1,25 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}
+, revCount ? 0
+, shortRev ? "0000000"
+, gitTag ? ""
+}:
 
 let
   buildEnv = pkgs.python3.withPackages (ps: [ ps.sphinx ps.sphinx_rtd_theme ]);
+  version = "${toString revCount}.${shortRev}";
 
 in pkgs.stdenv.mkDerivation {
   name = "flyingcircus-docs";
   configurePhase = ":";
   buildInputs = [ buildEnv ] ++ (with pkgs; [ python3 git ]);
   doCheck = false;
-  buildPhase = "sphinx-build -j 10 -a -b html $src/src $out";
+  buildPhase = ''
+    cd $src
+    sphinx-build -j 10 -a -b html src $out
+  '';
   installPhase = ":";
   src = ./.;
   dontStrip = true;
   dontPatchELF = true;
+  inherit revCount shortRev gitTag version;
 }
--- release.nix
+++ release.nix
@@ -1,13 +1,31 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}
+, doc ? {
+    outPath = ./.;
+    revCount = 0;
+    shortRev = "0000000";
+    gitTag = "master";
+  }
+}:
 let
   lib = pkgs.lib;
-  doc = import ./. { inherit pkgs; };
+
 in {
   platformDoc = lib.hydraJob (
-    pkgs.runCommandLocal "platform-doc" { inherit doc; } ''
-      mkdir -p $out/nix-support
-      tar czf $out/platform-doc.tar.gz -C $doc .
-      cp $doc/objects.inv $out
-      echo "file objects $out/objects.inv" > $out/nix-support/hydra-build-products
-    '');
+    pkgs.stdenv.mkDerivation {
+      name = "platform-doc";
+      src = (import ./. {
+        inherit pkgs;
+        inherit (doc) revCount shortRev gitTag;
+      });
+      builder = pkgs.stdenv.shell;
+      PATH = with pkgs; lib.makeBinPath [ coreutils gnutar gzip ];
+      args = [ "-ec" ''
+        mkdir -p $out/nix-support
+        tar czf $out/platform-doc.tar.gz -C $src .
+        echo "file tarball $out/platform-doc.tar.gz" >> $out/nix-support/hydra-build-products
+        cp $src/objects.inv $out
+        echo "file objects $out/objects.inv" >> $out/nix-support/hydra-build-products
+      ''];
+    }
+  );
 }
